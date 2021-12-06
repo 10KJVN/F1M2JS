dc12f729efc5c0c96a0ea199f0e98de226dff72e
Jahvairo <33427@ma-web.nl>
2021-12-06 12:44:53 +0100

    Les 1 klaar

---
+++ JSBasics/basics.js
@@ -0,0 +1,29 @@
+//while for if functions var lists arranys
+
+console.log("hallo world");
+
+var hallo //bestaat buiten de scope
+let halloo  //bestaat alleen binnen de scope
+
+let a = 9;
+let b = 2;
+let c = 50;
+if(a > 10 && b > 1){    //and && or ||
+    let x = 10;
+
+}
+else if(c==50){     //als de if niet waar is
+
+}else{              //als niets waar was
+
+}
+x++;
+
+let i = 0;
+while(i < 9000){        //doe iets zolang
+    console.log("boe");
+    i++;
+}
+for(let i=0;1<9000;i++){    //doe iets een
+    console.log("boe");
+}
